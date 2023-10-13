4aa0f0c4341b79ab94a8402ab369a01ada6c0c01
Diego Sanchez <diegosanchez.2340@gmail.com>
2023-10-13 10:27:17 -0500

    Automatic reading .asc

--- asc2png/script.py
+++ asc2png/script.py
@@ -1,5 +1,20 @@
-import lt2circuitikz.lt2ti as asc2tex;
-file = r'Prueba.asc';
-a2tobj = asc2tex.lt2circuiTikz();
-a2tobj.readASCFile(file);
-a2tobj.writeCircuiTikz(file+r'.tex');
+import lt2circuitikz.lt2ti as asc2tex
+import os
+
+carpeta = "../data_generator/ascs"
+archivos = os.listdir(carpeta)
+
+#archivos_tex = []
+#archivos_tex = archivos_tex.append([x for x in archivos if x.endswith('.tex')])
+archivos_tex = [x for x in archivos if x.endswith('.asc')]
+print(archivos_tex)
+
+for k in archivos_tex:
+    a2tobj = asc2tex.lt2circuiTikz()
+    ruta = "../data_generator/ascs/"+k
+    a2tobj.readASCFile(ruta)
+    a2tobj.writeCircuiTikz(k+r'.tex')
+
+'''a2tobj = asc2tex.lt2circuiTikz()
+a2tobj.readASCFile("..\data_generator\ascs\Prueba.asc")
+a2tobj.writeCircuiTikz("pruebaa"+'.tex')'''
