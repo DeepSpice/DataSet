b93971bf15aa730ff6686b505a96386e88f9416f
FaSaSu20 <fasuareza@unal.edu.co>
2023-11-10 09:54:06 -0500

    Genración carpetas
    - Generacion de archivos png's a partir de asc

--- asc2png/main_asc2png.py
+++ asc2png/main_asc2png.py
@@ -2,12 +2,27 @@ from asc2tex import asc2tex
 from tex2pdf import tex2pdf
 from pdf2png import pdf2png
 import time
+import shutil
 import os
 
 pathASC = "../Data/ascs/"
 pathTEMPtex = "../Data/temp/tex/"
 pathTEMPpdf = "../Data/temp/pdf/"
 pathPNG = "../Data/pngs"
+
+def create_folder(folder_path):
+    # Check if the folder already exists
+    if not os.path.exists(folder_path):
+        # If not, create it
+        os.makedirs(folder_path)
+        print(f"Folder '{folder_path}' created.")
+    else:
+        print(f"Folder '{folder_path}' already exists.")
+
+create_folder(pathASC)
+create_folder(pathTEMPtex)
+create_folder(pathTEMPpdf)
+create_folder(pathPNG)
 
 def asc2png(name):
     toTEX = asc2tex(pathASC + name +".asc", pathTEMPtex)
@@ -27,10 +42,12 @@ filesASC = os.listdir(pathASC)
 #Convert each .asc file to .tex format
 for k in filesASC:
 #    #Delete on the name the part that says ".asc"
-     name = k.replace('.asc', '')
-     asc2png(name)
+    name = k.replace('.asc', '')
+    asc2png(name)
 
-os.remove(pathTEMPtex + "latex_ext.tex")
+# os.remove(pathTEMPtex + "latex_ext.tex")
+
+shutil.rmtree("../Data/temp/")
 
 end_time = time.time()
 elapsed_time = end_time - start_time
