495017ac923c55e6092e43e2a1a2bf4f7f5a12a1
camsp <czambrano@unal.edu.co>
2025-01-26 20:11:52 -0500

    use dijkstra for wire generation (WIP)

--- circuit_generator/create_asc.py
+++ circuit_generator/create_asc.py
@@ -2,16 +2,47 @@ import numpy as np
 import random
 import sys
 from tqdm import tqdm
+#from LTSpice_generator import schematicAscGenerator
 from circuit_generator.LTSpice_generator import schematicAscGenerator
+from networkx import grid_graph, dijkstra_path
 
-def create_asc(n=1, path=""):
+def simplify_trajectory(trajectory):
+    # Lista para almacenar los nodos simplificados
+    simplified_nodes = []
+    
+    # Dirección previa inicializada como None
+    direccion_previa = None
+    
+    for i in range(len(trajectory) - 1):
+        # Calculamos la dirección actual como un vector (diferencia entre nodos consecutivos)
+        direccion_actual = (
+            trajectory[i + 1][0] - trajectory[i][0],
+            trajectory[i + 1][1] - trajectory[i][1]
+        )
+        
+        # Si la dirección actual es diferente de la dirección previa, añadimos el nodo actual
+        if direccion_actual != direccion_previa:
+            simplified_nodes.append(trajectory[i])
+            direccion_previa = direccion_actual
+    
+    # Añadimos el último nodo de la trayectoria
+    simplified_nodes.append(trajectory[-1])
+    
+    return simplified_nodes
+
+def create_asc(n=1, save_path=""):
     generator = schematicAscGenerator()
     elements = generator.getComponents()
     orientation = [0, 90]
+
     for i in tqdm(range(n)):
         n_elements = random.randint(2, 10)
         n_nodes = [i for i in range(random.randint(2, n_elements))]        
         positions = []
+
+        G = grid_graph(dim=(100, 100))
+
+
         for element in range(n_elements):
             node_i = random.choice(n_nodes)
             node_f = random.choice(np.delete(n_nodes, node_i))
@@ -41,6 +72,11 @@ def create_asc(n=1, path=""):
             })
 
         init_end = [ [i["node_i"], i["node_f"], i["element"]] for i in positions]
+        
+
+        G = grid_graph(dim=(100, 100))
+
+        #G.remove_edge(node_i, node_f)
 
         coords = generator.getCoords()
         for node in n_nodes:
@@ -55,10 +91,21 @@ def create_asc(n=1, path=""):
                     y_fin = coords[element]["end_y"]
                     items.append([x_fin, y_fin])
 
+
             for index, item in enumerate(items):
                 if index == 0:
                     continue
-                generator.wire(item[0], item[1], items[0][0], items[0][1])
-        generator.compile(path+"circuit_"+str(i)+".asc")
+                path = dijkstra_path(G, (item[0]/16, item[1]/16), (items[0][0]/16, items[0][1]/16))
+                simply_path = simplify_trajectory(path)
+                
+                for node_index, path_nodes in enumerate(simply_path):
+                    generator.wire(path_nodes[0]*16, path_nodes[1]*16, simply_path[node_index+1][0]*16, simply_path[node_index+1][1]*16)
+                    if(node_index+1 >= len(simply_path)-1):
+                        break
+
+        generator.compile(save_path+"circuit_"+str(i)+".asc")
         
 # sys.modules[__name__] = create_asc
+
+if __name__ == "__main__":
+    create_asc()
