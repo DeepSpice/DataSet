31c5da6019cf893dc22c7c23db4075a0c0be5534
Mastercode26 <juanherrera7az@gmail.com>
2025-02-01 17:52:53 -0500

    Update LTSpice_generator.py
    Se supone que se optimizo, no pude probarlo porque a pesar de que lo corro en mi terminal no me genera las imágenes (falta probarlo, recemos para que salga bien).

--- circuit_generator/LTSpice_generator.py
+++ circuit_generator/LTSpice_generator.py
@@ -1,100 +1,102 @@
 import re
 import numpy as np
 
-class schematicAscGenerator:
+class SchematicAscGenerator:
     def __init__(self):
-        self.wires = {}
-        self.g = {}
-        self.r = {}
-        self.c = {}
-        self.l = {}
-        self.d = {}
-        self.v = {}
-        self.comp = {}
+        self.components = {
+            'wires': {},
+            'g': {},
+            'r': {},
+            'c': {},
+            'l': {},
+            'd': {},
+            'v': {},
+            'comp': {}
+        }
         self.coords = []
-
         self.padding = 16
-
         # File header
         self.asc = "Version 4\nSHEET 1 880 680\n"
-    
+
     def reset(self):
-        self.wires = {}
-        self.g = {}
-        self.r = {}
-        self.c = {}
-        self.l = {}
-        self.d = {}
-        self.v = {}
-        self.comp = {}
+        for key in self.components:
+            self.components[key] = {}
         self.coords = []
         self.asc = "Version 4\nSHEET 1 880 680\n"
 
-        
-    def coordsSetter(self, x, y, comp="wire", deg = 0):
-        newX = x if x%self.padding == 0 else round(x/self.padding)*self.padding
-        newY = y if y%self.padding == 0 else round(y/self.padding)*self.padding
-        #newY = -1*newY
+    def coords_setter(self, x, y, comp="wire", deg=0):
+        new_x = round(x / self.padding) * self.padding if x % self.padding != 0 else x
+        new_y = round(y / self.padding) * self.padding if y % self.padding != 0 else y
 
-        if comp == "res" or comp == "ind":
-            newX = newX-self.padding*np.sign(np.cos(np.deg2rad(deg+1)))
-            newY = newY-self.padding*np.sign(np.sin(np.deg2rad(deg+1)))
-        if comp == "diode":
-            newX = newX-self.padding*round(np.cos(np.deg2rad(deg)))
-            newY = newY-self.padding*round(np.sin(np.deg2rad(deg)))
-        if comp == "cap":
-            newX = newX-self.padding*round(np.cos(np.deg2rad(deg)))
-            newY = newY-self.padding*round(np.sin(np.deg2rad(deg)))
-        if comp == "volt":
-            newX = newX+self.padding*round(np.sin(np.deg2rad(deg)))
-            newY = newY-self.padding*round(np.cos(np.deg2rad(deg)))
-        if comp == "curr":
-            newX = newX
-            newY = newY
-        if comp == "wire":
-            newX = newX
-            newY = newY
-        return round(newX), round(newY)
-
-
-    def create components(self,comp_type, x, y, deg, val= None, extra_config=None)
-        #para crear una sola función general para todos los componentes
-        #ahora hay que un nombre unico basado en cada uno de los componentes 
-
-        component_info = {
-            'res' : (F'R{len(self.r)}', self.r, "SYMBOL res", val), #Resistence
-            'cap' : (F'C{len(self.c)', self.c, "SYMBOL cap", val), #Capacitor
-            'ind' : (F'L{len(self.l)', self.l, "SYMBOL ind", val), #Inductor 
-            'diode': (F'D{len(self.d)', self.d, "SYMBOL diode", val), #Diode
-            'volt': (F'V{len(self.v)', self.v, "SYMBOL volt", val), #Voltage
-            'current': (F'C{len(self.c)', self.c, "SYMBOL current", val), #Current
+        adjustments = {
+            "res": (-np.sign(np.cos(np.deg2rad(deg + 1))), -np.sign(np.sin(np.deg2rad(deg + 1)))),
+            "ind": (-np.sign(np.cos(np.deg2rad(deg + 1))), -np.sign(np.sin(np.deg2rad(deg + 1)))),
+            "diode": (-round(np.cos(np.deg2rad(deg))), -round(np.sin(np.deg2rad(deg)))),
+            "cap": (-round(np.cos(np.deg2rad(deg))), -round(np.sin(np.deg2rad(deg)))),
+            "volt": (round(np.sin(np.deg2rad(deg))), -round(np.cos(np.deg2rad(deg)))),
+            "curr": (0, 0),
+            "wire": (0, 0)
         }
+
+        if comp in adjustments:
+            adj_x, adj_y = adjustments[comp]
+            new_x += self.padding * adj_x
+            new_y += self.padding * adj_y
+
+        return round(new_x), round(new_y)
+
+    def create_component(self, comp_type, x, y, deg, val=None, extra_config=None):
+        component_info = {
+            'res': (f'R{len(self.components["r"])}', self.components['r'], "SYMBOL res", val, "resistance"),
+            'cap': (f'C{len(self.components["c"])}', self.components['c'], "SYMBOL cap", val, "capacitance"),
+            'ind': (f'L{len(self.components["l"])}', self.components['l'], "SYMBOL ind", val, "inductance"),
+            'diode': (f'D{len(self.components["d"])}', self.components['d'], "SYMBOL diode", val, None),
+            'volt': (f'V{len(self.components["v"])}', self.components['v'], "SYMBOL volt", val, "voltage"),
+            'current': (f'I{len(self.components["comp"])}', self.components['comp'], "SYMBOL current", val, "current")
+        }
+
         if comp_type not in component_info:
             return
-        
-                        
-            
 
-              
-    
-    def wire(self, x0, y0, x1, y1):
+        name, comp_dict, symbol, value, value_tag = component_info[comp_type]
+        x1, y1 = self.coords_setter(x, y, comp_type, deg)
+        header = f'{symbol} {x1} {y1} R{deg}'
+        name_tag = f'SYMATTR InstName {name}'
+        value_tag = f'SYMATTR Value {value}'
+
+        comp_dict[name] = {
+            "header": header,
+            "nameTag": name_tag,
+            "valueTag": value_tag
+        }
+
+        def wire(self, x0, y0, x1, y1):
+        header = f'{symbol} {x1} {y1} R{deg}'
         #'''
+        name_tag = f'SYMATTR InstName {name}'
         # x0 = initial position in x
+        value_tag = f'SYMATTR Value {value}'
         # y0 = initial position in y
         # x1 = final position in x
         # y1 = final position in y
         # '''
         x0, y0 = self.coordsSetter(x0, y0)
         x1, y1 = self.coordsSetter(x1, y1)
-
+ 
+ 
         if (x0 == x1) or (y0 == y1): ### Cable recto
+        comp_dict[name] = {
             name = f'W{len(self.wires)}'        
+            "header": header,
             self.wires[name] = f'WIRE {x0:-} {y0:-} {x1:-} {y1:-}'
+            "nameTag": name_tag,
         else:
+            "valueTag": value_tag
             name = f'W{len(self.wires)}'
             self.wires[name] = f'WIRE {x0:-} {y0:-} {x1:-} {y0:-}\nWIRE {x1:-} {y0:-} {x1:-} {y1:-}'
-    
-    def ground(self, x, y):
+
+
+        def ground(self, x, y):
         # x = initial position in x
         # y = initial position in y
         x, y = self.coordsSetter(x, y)
@@ -107,240 +109,85 @@ class schematicAscGenerator:
             "end_y": y, 
         })
 
-            
-    def res(self, x, y, deg, val):
-        # x = initial position in x
-        # y = initial position in y
-        # deg = rotation (0, 90, 270)
-        # val = resistance value
-        x1, y1 = self.coordsSetter(x, y, "res", deg)
-        # Name of resistance is equal to the number of components 
-        name = f'R{len(self.r)}'
-        # Coords and rotation
-        header = f'SYMBOL res {x1} {y1} R{deg}'
-        # Box sizes
-        window = ""
-        # Resistance name
-        nameTag = f'SYMATTR InstName {name}'
-        # Resistance value
-        valueTag = f'SYMATTR Value {val}'
-        # Object with info of resistance
-        self.r[name] = {
-            "header":header,
-            "nameTag":nameTag,
-            "valueTag":valueTag
-        }
+        if deg != 0:
+            window_sizes = {
+                90: "WINDOW 0 0 32 VBottom 2\nWINDOW 3 32 32 VTop 2",
+                180: "WINDOW 0 24 56 VLeft 2\nWINDOW 3 24 8 VLeft 2",
+                270: "WINDOW 0 32 32 VTop 2\nWINDOW 3 0 32 VBottom 2"
+            }
+            if comp_type in ['volt', 'current']:
+                window_sizes = {
+                    0: "WINDOW 123 0 0 VLeft 0\nWINDOW 39 0 0 VLeft 0",
+                    90: "WINDOW 0 -32 56 VBottom 2\nWINDOW 3 32 56 VTop 2\nWINDOW 123 0 0 VLeft 0\nWINDOW 39 0 0 VLeft 0",
+                    180: "WINDOW 0 24 96 VLeft 2\nWINDOW 3 24 16 VLeft 2\nWINDOW 123 0 0 VLeft 0\nWINDOW 39 0 0 VLeft 0",
+                    270: "WINDOW 0 32 56 VTop 2\nWINDOW 3 -32 56 VBottom 2\nWINDOW 123 0 0 VLeft 0\nWINDOW 39 0 0 VLeft 0"
+                }
+            comp_dict[name]["window"] = window_sizes.get(deg, "")
 
         self.coords.append({
-            "start_x": x, 
-            "start_y": y, 
-            "end_x": x-round(np.sin(np.deg2rad(deg)))*self.padding*5, 
-            "end_y": y+round(np.cos(np.deg2rad(deg)))*self.padding*5, 
+            "start_x": x,
+            "start_y": y,
+            "end_x": x - round(np.sin(np.deg2rad(deg))) * self.padding * 4,
+            "end_y": y + round(np.cos(np.deg2rad(deg))) * self.padding * 4,
         })
 
-        # Change window size depends of rotation
-        if deg == 90 or deg==270:
-            window  = "WINDOW 0 0 56 VBottom 2\nWINDOW 3 32 56 VTop 2" if deg == 90 else "WINDOW 0 32 56 VBottom 2\nWINDOW 3 0 56 VTop 2"
-            self.r[name]["window"] = window
-            
-    def cap(self, x, y, deg, val):
-        # x = initial position in x
-        # y = initial position in y
-        # deg = rotation (0, 90, 270)
-        # val = capacitor value
-        x1, y1 = self.coordsSetter(x, y, "cap", deg)
-        # Name of capacitor is equal to the number of components 
-        name = f'C{len(self.c)}'
-        # Coords and rotation
-        header = f'SYMBOL cap {x1} {y1} R{deg}'
-        # Box sizes
-        window = ""
-        # Resistance name
-        nameTag = f'SYMATTR InstName {name}'
-        # Resistance value
-        valueTag = f'SYMATTR Value {val}'
-        # Object with info of capacitor
-        self.c[name] = {
-            "header":header,
-            "nameTag":nameTag,
-            "valueTag":valueTag
-        }
-        # Change window size depends of rotation
-        if deg != 0:
-            if deg == 90:
-                window  = "WINDOW 0 0 32 VBottom 2\nWINDOW 3 32 32 VTop 2"
-            elif deg == 180:
-                window  = "WINDOW 0 25 56 VLeft 2\nWINDOW 3 24 8 VLeft 2"
-            elif deg == 270:
-                window  = "WINDOW 0 32 32 VTop 2\nWINDOW 3 0 32 VBottom 2"
-            self.c[name]["window"] = window
-        
+    def wire(self, x0, y0, x1, y1):
+        x0, y0 = self.coords_setter(x0, y0)
+        x1, y1 = self.coords_setter(x1, y1)
+
+        name = f'W{len(self.components["wires"])}'
+        if x0 == x1 or y0 == y1:
+            self.components["wires"][name] = f'WIRE {x0:-} {y0:-} {x1:-} {y1:-}'
+        else:
+            self.components["wires"][name] = f'WIRE {x0:-} {y0:-} {x1:-} {y0:-}\nWIRE {x1:-} {y0:-} {x1:-} {y1:-}'
+
+    def ground(self, x, y):
+        x, y = self.coords_setter(x, y)
+        name = f'G{len(self.components["g"])}'
+        self.components["g"][name] = f'FLAG {x:-} {y:-} 0'
         self.coords.append({
-            "start_x": x, 
-            "start_y": y, 
-            "end_x": x-round(np.sin(np.deg2rad(deg)))*self.padding*4, 
-            "end_y": y+round(np.cos(np.deg2rad(deg)))*self.padding*4, 
-        })
-            
-    def ind(self, x, y, deg, val):
-        # x = initial position in x
-        # y = initial position in y
-        # deg = rotation (0, 90, 270)
-        # val = capacitor value
-        x1, y1 = self.coordsSetter(x, y, "ind", deg)
-        # Name of capacitor is equal to the number of components 
-        name = f'L{len(self.l)}'
-        # Coords and rotation
-        header = f'SYMBOL ind {x1} {y1} R{deg}'
-        # Box sizes
-        window = ""
-        # Resistance name
-        nameTag = f'SYMATTR InstName {name}'
-        # Resistance value
-        valueTag = f'SYMATTR Value {val}'
-        # Object with info of capacitor
-        self.l[name] = {
-            "header":header,
-            "nameTag":nameTag,
-            "valueTag":valueTag
-        }
-        # Change window size depends of rotation
-        if deg != 0:
-            if deg == 90:
-                window  = "WINDOW 0 5 56 VBottom 2\nWINDOW 3 32 56 VTop 2"
-            elif deg == 180:
-                window  = "WINDOW 0 36 80 VLeft 2\nWINDOW 3 36 40 VLeft 2"
-            elif deg == 270:
-                window  = "WINDOW 0 32 56 VTop 2\nWINDOW 3 5 56 VBottom 2"
-            self.l[name]["window"] = window
-        self.coords.append({
-            "start_x": x, 
-            "start_y": y, 
-            "end_x": x-round(np.sin(np.deg2rad(deg)))*self.padding*5, 
-            "end_y": y+round(np.cos(np.deg2rad(deg)))*self.padding*5, 
-        })
-                       
-    def diode(self, x, y, deg):
-        # x = initial position in x
-        # y = initial position in y
-        # deg = rotation (0, 90, 270)
-        # val = capacitor value
-        x1, y1 = self.coordsSetter(x, y, "diode", deg)
-        # Name of capacitor is equal to the number of components 
-        name = f'D{len(self.d)}'
-        # Coords and rotation
-        header = f'SYMBOL diode {x1} {y1} R{deg}'
-        # Box sizes
-        window = ""
-        # Resistance name
-        nameTag = f'SYMATTR InstName {name}'
-        # Object with info of capacitor
-        self.d[name] = {
-            "header":header,
-            "nameTag":nameTag,
-        }
-        # Change window size depends of rotation
-        if deg != 0:
-            if deg == 90:
-                window  = "WINDOW 0 0 32 VBottom 2\nWINDOW 3 32 32 VTop 2"
-            elif deg == 180:
-                window  = "WINDOW 0 24 64 VLeft 2\nWINDOW 3 24 0 VLeft 2"
-            elif deg == 270:
-                window  = "WINDOW 0 32 32 VTop 2\nWINDOW 3 0 32 VBottom 2"
-            self.d[name]["window"] = window
-        self.coords.append({
-            "start_x": x, 
-            "start_y": y, 
-            "end_x": x-round(np.sin(np.deg2rad(deg)))*self.padding*4, 
-            "end_y": y+round(np.cos(np.deg2rad(deg)))*self.padding*4, 
-        })
-                     
-    def voltage(self, x, y, deg, val):
-        # x = initial position in x
-        # y = initial position in y
-        # deg = rotation (0, 90, 270)
-        # val = capacitor value
-        x1, y1 = self.coordsSetter(x, y, "volt", deg)
-        # Name of capacitor is equal to the number of components 
-        name = f'V{len(self.v)}'
-        # Coords and rotation
-        header = f'SYMBOL voltage {x1} {y1} R{deg}'
-        # Box sizes
-        window = ""
-        # Resistance name
-        nameTag = f'SYMATTR InstName {name}'
-        # Resistance value
-        valueTag = f'SYMATTR Value {val}'
-        # Change window size depends of rotation
-        
-        if deg == 0:
-            window  = "WINDOW 123 0 0 VLeft 0\nWINDOW 39 0 0 VLeft 0"
-        elif deg == 90:
-            window  = "WINDOW 0 -32 56 VBottom 2\nWINDOW 3 32 56 VTop 2\nWINDOW 123 0 0 VLeft 0\nWINDOW 39 0 0 VLeft 0"
-        elif deg == 180:
-            window  = "WINDOW 0 24 96 VLeft 2\nWINDOW 3 24 16 VLeft 2\nWINDOW 123 0 0 VLeft 0\nWINDOW 39 0 0 VLeft 0"
-        elif deg == 270:
-            window  = "WINDOW 0 32 56 VTop 2\nWINDOW 3 -32 56 VBottom 2\nWINDOW 123 0 0 VLeft 0\nWINDOW 39 0 0 VLeft 0"
-        # Object with info of capacitor
-        self.v[name] = {
-            "header":header,
-            "nameTag":nameTag,
-            "valueTag":valueTag,
-            "window":window
-        }
-        self.coords.append({
-            "start_x": x, 
-            "start_y": y, 
-            "end_x": x-round(np.sin(np.deg2rad(deg)))*self.padding*5, 
-            "end_y": y+round(np.cos(np.deg2rad(deg)))*self.padding*5, 
+            "start_x": x,
+            "start_y": y,
+            "end_x": x,
+            "end_y": y,
         })
 
     def current(self, x, y, deg, val):
-        # x = initial position in x
-        # y = initial position in y
-        # deg = rotation (0, 90, 270)
-        # val = capacitor value
-        x1, y1 = self.coordsSetter(x, y)
-        # Name of capacitor is equal to the number of components 
+        x1, y1 = self.coords_setter(x, y)
         name = f'I{len(self.v)}'
-        # Coords and rotation
         header = f'SYMBOL current {x1} {y1} R{deg}'
-        # Box sizes
+        name_tag = f'SYMATTR InstName {name}'
+        value_tag = f'SYMATTR Value {val}'
+
         window = ""
-        # Resistance name
-        nameTag = f'SYMATTR InstName {name}'
-        # Resistance value
-        valueTag = f'SYMATTR Value {val}'
-        # Change window size depends of rotation
-        
         if deg == 90:
-            window  = "WINDOW 0 -32 40 VBottom 2\nWINDOW 3 32 40 VTop "
+            window = "WINDOW 0 -32 40 VBottom 2\nWINDOW 3 32 40 VTop 2"
         elif deg == 180:
-            window  = "WINDOW 0 24 80 Left 2\nWINDOW 3 24 0 Left "
+            window = "WINDOW 0 24 80 VLeft 2\nWINDOW 3 24 0 VLeft 2"
         elif deg == 270:
-            window  = "WINDOW 0 32 40 VTop 2\nWINDOW 3 -32 40 VBottom "
+            window = "WINDOW 0 32 40 VTop 2\nWINDOW 3 -32 40 VBottom 2"
+            
         # Object with info of capacitor
         self.v[name] = {
-            "header":header,
-            "nameTag":nameTag,
-            "valueTag":valueTag,
-            "window":window
+            "header": header,
+            "nameTag": name_tag,
+            "valueTag": value_tag,
+            "window": window
         }
         self.coords.append({
-            "start_x": x, 
-            "start_y": y, 
-            "end_x": x-round(np.sin(np.deg2rad(deg)))*self.padding*5, 
-            "end_y": y+round(np.cos(np.deg2rad(deg)))*self.padding*5, 
+            "start_x": x,
+            "start_y": y,
+            "end_x": x - round(np.sin(np.deg2rad(deg))) * self.padding * 5,
+            "end_y": y + round(np.cos(np.deg2rad(deg))) * self.padding * 5,
         })
-        
-    def Component(self, x, y, deg, compName):
-        x1, y1 = self.coordsSetter(x, y)
-        # For mosfets name starts with M
-        if compName == "nmos" or compName == "pmos":
+
+    def component(self, x, y, deg, comp_name):
+        x1, y1 = self.coords_setter(x, y)
+        if comp_name in ["nmos", "pmos"]:
             name = f'M{len(self.comp)}'
-        # For bjts name starts with Q
-        elif compName == "npn" or compName == "pnp":
-            name = f'Q{len(self.comp)}'
+        elif comp_name in ["npn", "pnp"]:
+
+         
 
         # For opamps name starts with OpAmps and add library
         elif compName == "opamp" or compName == "UniversalOpAmp":
@@ -395,4 +242,4 @@ class schematicAscGenerator:
             f.close()
         self.reset()
         
-    #def draw():
+
